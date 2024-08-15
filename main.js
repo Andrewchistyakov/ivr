ce39f2db55a0ee3c5e115403069dc96443352c9e
andrew <chistyakgg@gmail.com>
2024-08-15 16:21:02 +0300

    commit message

---
+++ main.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const url = require('url');
+const { app, BrowserWindow } = require('electron');
+
+
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 700,
+        height: 500,
+        icon: __dirname + "/img/icon.png"})
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    
+    win.webContents.openDevTools();
+
+    win.on('closed', () => win = null);
+
+
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => app.quit());
