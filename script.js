3f8cf4c22486ff66739cc8b58f6f3474c172dc81
andrew <chistyakgg@gmail.com>
2024-08-20 02:20:33 +0300

    implemented a basic todolist without storage

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function buttonHandler() {
-    console.log('button clicked!')
+const addTask = function() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value;
+    const taskList = document.getElementById("task-list");
+
+    if (taskText === '') { // checking if the submitted input is empty and alerting if so
+        alert("You did not enter your task :(");
+        return;
+    }
+
+    const newTask = document.createElement('li'); // creating a list item
+    newTask.textContent = taskText; // add the text we got from the input field to the task
+    
+    
+    const removeButton = document.createElement('button');  // creating a button to remove tasks
+    removeButton.textContent = 'DONE';
+    removeButton.onclick = () => newTask.remove(); // remove a task when clicked
+    newTask.appendChild(removeButton);
+
+    taskInput.value = ''; // clearing input field
+    taskList.appendChild(newTask); // adding a task
 }
