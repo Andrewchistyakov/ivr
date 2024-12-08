588898de27f800d921b85ab21156d94624a062ec
andrew <chistyakgg@gmail.com>
2024-12-08 15:53:29 +0300

    basic structure

---
+++ backend/Repository/UserStatsRepository.java
@@ -0,0 +1,6 @@
+package Repository;
+
+// TODO: user repo (спросить у германа на фд как должен выглядеть)
+public class UserStatsRepository {
+
+}
---
+++ backend/Service/UserService.java
@@ -0,0 +1,5 @@
+package Service;
+
+// TODO: узнать у германа на фд как должен выглядеть сервис
+public class UserService {
+}
